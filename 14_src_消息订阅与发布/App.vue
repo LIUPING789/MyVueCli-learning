fcba98662c18807f0df4dd776ba2cc772aa942ad
liuping <37442216knight@gmail.com>
2025-11-03 10:33:48 +0800

    使用pubsub-js实现消息订阅与发布

---
+++ 14_src_消息订阅与发布/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- 这里必须有一个根元素 -->
+  <div class="app">
+    <h1>{{ msg }}</h1>
+    <School></School>
+    <hr />
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+//引用组件
+import Student from "./components/Student.vue";
+import School from "./components/School.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      msg: "学习Vue！",
+    };
+  },
+
+  //注册组件
+  components: {
+    Student,
+    School,
+  },
+};
+</script>
+
+<style>
+.app {
+  background-color: blue;
+}
+</style>
---
+++ 14_src_消息订阅与发布/main.js
@@ -0,0 +1,15 @@
+
+//引入Vue
+import Vue from "vue";
+
+//引入组件
+import App from "./App.vue";
+
+//关闭生产模式下给出的提示
+Vue.config.productionTip = false;
+//创建Vue实例
+new Vue({
+  el: "#app",
+  render: h => h(App),
+
+})
