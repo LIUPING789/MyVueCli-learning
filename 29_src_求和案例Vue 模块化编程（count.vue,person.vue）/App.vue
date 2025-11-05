f195b3ff1511bc450399e64eb66e9938c73b89cf
liuping <37442216knight@gmail.com>
2025-11-05 16:47:50 +0800

    求和案例vue 模块化编程

---
+++ 29_src_求和案例Vue 模块化编程（count.vue,person.vue）/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- 这里必须有一个根元素 -->
+  <div>
+    <Count></Count>
+    <hr />
+    <Person></Person>
+  </div>
+</template>
+
+<script>
+import Count from "@/components/Count";
+import Person from "@/components/Person";
+
+//引入事件总线
+export default {
+  name: "App",
+  components: {
+    Count,
+    Person,
+  },
+};
+</script>
+
