78922d1cd448d20544e04aa622a59735a0de22db
liuping <37442216knight@gmail.com>
2025-11-06 15:24:59 +0800

    路由-全局前置守卫与全局后置守卫

---
+++ 40_src_路由中配制前置全局守卫与后置全局守卫/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="row">
+      <Banner></Banner>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-2 col-xs-offset-2">
+        <div class="list-group">
+          <!-- 原始html 中我们使用a 标签实现页面跳转 -->
+          <!--            
+          <a class="list-group-item active" href="./about.html">About</a>
+          <a class="list-group-item" href="./home.html">Home</a> 
+          -->
+
+          <!-- Vue Router 中使用router-link 实现页面跳转   -->
+          <router-link
+            replace
+            class="list-group-item"
+            active-class="active"
+            to="/about"
+            >About</router-link
+          >
+          <router-link
+            replace 
+            class="list-group-item"
+            active-class="active"
+            to="/home"
+            >Home</router-link
+          >
+        </div>
+      </div>
+
+      <div class="col-xs-6">
+        <div class="panel">
+          <!-- 指定路由组件，渲染对应路由页面 -->
+          <div class="panel-body">
+            <router-view></router-view>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Banner from "./components/Banner.vue";
+export default {
+  name: "App",
+  components: {
+    Banner,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 40_src_路由中配制前置全局守卫与后置全局守卫/pages/News.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <ul>
+      <li :style="{ opacity }">欢迎学习Vue</li>
+      <li>news001 <input type="text" /></li>
+      <li>news002 <input type="text" /></li>
+      <li>news003 <input type="text" /></li>
+      <li>news004 <input type="text" /></li>
+      <li>news005 <input type="text" /></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "News",
+
+  data() {
+    return {
+      opacity: 1,
+    };
+  },
+
+  // beforeDestroy() {
+  //   clearInterval();
+  // },
+  // mounted() {
+  //   this.timer = setInterval(() => {
+  //     this.opacity -= 0.1;
+  //     if (this.opacity <= 0) this.opacity = 1;
+  //   }, 16);
+  // },
+  activated() {
+    //激活时执行
+
+    this.timer = setInterval(() => {
+      this.opacity -= 0.1;
+      if (this.opacity <= 0) this.opacity = 1;
+    }, 16);
+  },
+  deactivated() {
+    //销毁时执行
+    clearInterval(this.timer);
+  },
+};
+</script>
+
+<style>
+</style>
