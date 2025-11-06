6b4294f1fef1029e4e46e2186d61cef4536ce15b
liuping <37442216knight@gmail.com>
2025-11-06 11:08:56 +0800

    路由配制中的props中使用

---
+++ 35_src_路由配制中的props 使用/pages/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>我是About页面</h2>
+</template>
+
+<script>
+export default {
+  name: "About",
+  beforeDestroy() {
+    console.log("About页面销毁前执行");
+  },
+  mounted() {
+    console.log("About页面挂载完成了");
+  },
+};
+</script>
+
+<style>
+</style>
